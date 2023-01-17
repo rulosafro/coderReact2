0803165bd010814920cf9bee44543ee8624a78f4
Javier Ramírez <jramirez2432@gmail.com>
2023-01-17 09:56:23 -0300

    prueba vercel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='container mx-auto px-4 py-5 '>Footer</div>
+    <div className='container mx-auto px-4 py-5 '>Footer
+      <div>
+        <h2>El bulla es lo más grande</h2>
+      </div>
+    </div>
   )
 }
